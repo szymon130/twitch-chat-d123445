d6db9f654c236657986a110cd31b63e6065544b5
Szymon Gołąbek <szymon@ad.local>
2025-07-22 13:12:38 +0200

    Fixes

---
+++ src/components/RehydratedMessage.jsx
@@ -0,0 +1,63 @@
+// src/components/RehydratedMessage.jsx
+
+import React from 'react';
+import { Message } from '../helpers/Message';
+import Image from '../helpers/Image';
+import JSONViewer from './JSONViewer';
+
+// Ensure `state` and `dispatch` are passed down for `Message` component's internal logic
+const RehydratedMessage = ({ line, dispatch, state }) => {
+    const { type, content, rehydrateType, rehydrateData } = line;
+
+    if (rehydrateType === 'user_message_data') {
+        const { user, channel, formatted_time, message_part, channel_color, user_color, tags } = rehydrateData;
+        return (
+            <Message
+                user={user} channel={channel} formatted_time={formatted_time}
+                message_part={message_part} channel_color={channel_color}
+                user_color={user_color} tags={tags} dispatch={dispatch} state={state}
+            />
+        );
+    } else if (rehydrateType === 'joined_channels_data') {
+        const obj = rehydrateData;
+        return (
+            <div>
+                {obj.map(d => (
+                    <div className="ml-3" key={d.channel}>
+                        <span>{d.channel}</span>
+                        - ACTIVE: <span className={d.isLive === "YES" ? "pl-3 text-green-400" : "text-red-400"}> {d.isLive}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    } else if (rehydrateType === 'channel_joined_data') {
+        const { channel, image } = rehydrateData;
+        return (
+            <span>
+                Joined channel {channel}
+                <span className='ml-2'>
+                    <Image code={image.code} urls={image.urls} />
+                </span>
+            </span>
+        );
+    } else if (rehydrateType === 'channel_exited_data') {
+        const { channel, image } = rehydrateData;
+        return (
+            <span>
+                Left channel {channel}
+                <span className='ml-2'>
+                    <Image code={image.code} urls={image.urls} />
+                </span>
+            </span>
+        );
+    } else if (rehydrateType === 'debug_output') {
+        // As debug output is highly dynamic and might contain circular refs,
+        // it's generally not fully rehydrated from localStorage.
+        return '[Debug Output from previous session]';
+    }
+    // Fallback for simple string content or directly provided JSX (if any)
+    // This will render placeholders for generic JSX or function content after refresh
+    return content;
+};
+
+export default RehydratedMessage;
